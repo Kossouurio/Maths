af860eb2e6e362b9430cda79a9d8c422fc58742f
Kossouurio <ilankonan7@gmail.com>
2025-11-04 09:03:46 +0100

    1.2

--- Projet1/Projet1/Projet1.cpp
+++ Projet1/Projet1/Projet1.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
+#include <Windows.h>
+
+class Console
+{
+public:
+    HANDLE hConsole;
+    DWORD mode;
+    
+    Console()
+    {
+        hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        mode = DWORD();
+
+        GetConsoleMode(hConsole, &mode);
+        SetConsoleMode(hConsole, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+    }
+
+    void HideCursor()
+    {
+        std::cout << "\033[?25l";
+    }
+
+    void ShowCursor()
+    {
+        std::cout << "\033[?25h";
+    }
+    
+    void ResetCursortPosition()
+    {
+        std::cout << "\033[H";
+    }
+    
+    void Clear()
+    {
+        std::cout << "\033[2J";
+    }
+};
 
 int main(int argc, char* argv[])
 {
     std::cout << "Hello World!\n";
+    Console console;
+    console.ShowCursor();
+    
     return 0;
 }
